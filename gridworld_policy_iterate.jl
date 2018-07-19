38221d3a10d6e081537d2c2312dc8cbbbfd5309d
lizz <363664470@qq.com>
2018-07-19 15:05:51 +0800

    nice

--- gridworld_policy_iterate.jl
+++ gridworld_policy_iterate.jl
@@ -1,35 +1,54 @@
 
+# stop condition
+isend(s) = s in [(1, 1), (4, 4)]
+
 # transition
 function next_state(current_state, action)
     i, j = current_state
-    if action == ↑
+    if action == :↑
         j = max(1, j - 1)
-    elseif action == ↓
+    elseif action == :↓
         j = min(4, j + 1)
-    elseif action == ←
+    elseif action == :←
         i = max(1, i - 1)
-    elseif action == →
+    elseif action == :→
         i = min(4, i + 1)
     else
         error("action not valid")
     end
+    (i, j)
 end
 
 # display values and greedy policy
 function display_value(V)
     Vmat= zeros(4, 4)
     for ((i, j), v) in pairs(V)
-        Vmat[i, j] = v
+        Vmat[i, j] = round(v, digits=1)
     end
     println(join(split(repr("text/plain", Vmat), "\n")[2:end], "\n"))
 end
 
-function iterate(V)
+function iterate(V, A, γ)
     newV = copy(V)
-    for ((i, j), v) in pairs(V)
-        newV[(i, j)] = v
+    for (state, v) in pairs(V)
+        isend(state) && continue
+        newV[state] = reward(state)
+        for (a, p) in policy(state, A)
+            newV[state] += γ * p * V[next_state(state, a)]
+        end
     end
     newV
+end
+
+# policy: action, probability list
+function policy(state, A)
+    isend(state) && return Dict()
+    Dict(a => 1 / length(A) for a in A)
+end
+
+# reward
+function reward(state)
+    isend(state) ? 0 : -1
 end
 
 ## main
@@ -37,6 +56,7 @@ end
 function main()
     # config
     N = 10
+    γ = 1
     # states
     S = [(i, j) for i in 1:4 for j in 1:4]
     # actions
@@ -49,7 +69,7 @@ function main()
     display_value(V)
     for k in 1:N
         println("---------- $k ----------")
-        V = iterate(V)
+        V = iterate(V, A, γ)
         display_value(V)
     end
 
